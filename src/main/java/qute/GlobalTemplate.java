ce668070b983f1709b822a271977be8411db1e33
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2025-07-13 16:23:16 -0300

    Adjust date locale (#31)

--- src/main/java/qute/GlobalTemplate.java
+++ src/main/java/qute/GlobalTemplate.java
@@ -3,9 +3,10 @@ package qute;
 import io.quarkus.qute.TemplateGlobal;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @TemplateGlobal
 public class GlobalTemplate {
 
-    public static DateTimeFormatter FULL_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    public static DateTimeFormatter FULL_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.of("pt", "BR"));
 }
