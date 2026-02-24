3c13aa2624319f4e878ce5055aafc91de0368ba9
Matheus Cruz <56329339+mcruzdev@users.noreply.github.com>
2026-02-24 12:48:18 -0300

    Add welcome blog to Effective Java book club(#98)
    * Add welcome blog to Effective Java book club
    
    ---------
    
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

--- src/main/java/qute/GlobalTemplate.java
+++ src/main/java/qute/GlobalTemplate.java
@@ -8,5 +8,7 @@ import java.util.Locale;
 @TemplateGlobal
 public class GlobalTemplate {
 
-    public static DateTimeFormatter FULL_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.of("pt", "BR"));
+    public static DateTimeFormatter FULL_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm", Locale.of("pt", "BR"));
+
+    public static DateTimeFormatter ONLY_DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.of("pt", "BR"));
 }
