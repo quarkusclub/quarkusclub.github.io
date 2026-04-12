f6b866526dc39ea1a39d24136cac0a0589b7a094
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-04-12 14:13:22 +0530

    Update app.js

--- src/main/resources/web/app/app.js
+++ src/main/resources/web/app/app.js
@@ -3,3 +3,10 @@ import hljs from 'highlight.js';
 import 'highlight.js/styles/tokyo-night-dark.min.css';
 
 hljs.highlightAll();
+
+const btn = document.getElementById("theme-toggle");
+
+btn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  document.body.classList.toggle("light");
+});
